addfd62aca08561e74cc1fea813c9c3d67ffdb92
mmarini <marco.marini@mmarini.org>
2019-06-16 18:57:08 +0200

    plotAlpha

---
+++ octave/plotAlphas.m
@@ -0,0 +1,18 @@
+#function plotAlphas()
+  BASEPATH = "..";
+  COL = 1;
+  FNAMES = [
+    BASEPATH "/data-alpha-10";
+#    BASEPATH "/data-alpha-30";
+    BASEPATH "/data-base";
+#    BASEPATH "/data-alpha-300";
+#    BASEPATH "/data-alpha-1"
+  ];
+  LEGEND=[
+    "0.01",
+#    "0.03",
+    "0.1",
+#    "0.3",
+#    "1"
+  ];
+  plotStats(FNAMES, "alpha", LEGEND, 1);
---
+++ octave/plotStats.m
@@ -0,0 +1,28 @@
+function plotStats(FNAMES, TITLE, LEGEND, COL = 1)
+## -*- texinfo -*-
+## @deftypefn  {Function File} [ @var{RETURNS} @var{ERRORS} ] = plotStats(@var{FNAMES}, @var(TITLE), @var(LEGEND=, @var(COL) = 1)
+## Plot the statistica by hyer parameters
+## 
+## @var{FNAMEs} is the stats folders
+##
+## @var{TITLE} is the title of graph
+##
+## @var{LEGEND} is the legend for each hyper parameter value
+##
+## @var{COL} 1 for MEAN, 6 for MEDIAN
+## @end deftypefn
+  [RETURNS ERRORS] = compareStats(FNAMES, COL);
+  
+  subplot(2, 1, 1);
+  plot(RETURNS);
+  grid on;
+  title([TITLE " Returns" ]);
+  legend(LEGEND);
+  
+  subplot(2, 1, 2);
+  semilogy(ERRORS);
+  grid on;
+  title([TITLE " Errors" ]);
+  legend(LEGEND);
+  
+endfunction
