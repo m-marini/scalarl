eca0c42a3c889aab6f922e71069957d5465ff948
mmarini <marco.marini@mmarini.org>
2020-04-22 22:47:45 +0200

    data

--- runepochs.sh
+++ runepochs.sh
@@ -1,9 +1,8 @@
 #/bin/bash
-f="v1/lander-tiles-es-0.yaml" #$1
 n=99 #$2
 #sbt stage
 for i in {0..99}
 do
-    ./target/universal/stage/bin/org_mmarini_scalarl_v-1_envs_main $f $i
+    ./target/universal/stage/bin/org_mmarini_scalarl_v-1_envs_main $1 $i
     #sbt "runMain org.mmarini.scalarl.v1.envs.Main $f $i"
 done
